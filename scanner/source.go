945580f097a9642eb71ab384f9758edae358893d
PaulioRandall <29413800+PaulioRandall@users.noreply.github.com>
2019-11-09 20:38:06 +0000

    Add panic assertions to source.scan() for bad input
    NEXT: scan whitespace tokens except newlines

--- scanner/source.go
+++ scanner/source.go
@@ -13,8 +13,15 @@ type source struct {
 
 // scan removes `n` runes from the unscanned source code and uses them to
 // create a new token. The source line and column indexes are updated
-// accordingly.
+// accordingly. If `n` is less than one or greater than the number of remaining
+// runes then a panic ensues.
 func (s *source) scan(n int, k token.Kind) token.Token {
+
+	if n < 1 {
+		panic("Scanning a negative or zero number of runes is not allowed")
+	} else if n > len(s.runes) {
+		panic("Cannot scan more runes than are available")
+	}
 
 	t := token.Token{
 		Line:  s.line,
--- scanner/source_test.go
+++ scanner/source_test.go
@@ -27,3 +27,23 @@ func TestScan_1(t *testing.T) {
 	assert.Equal(t, 0, s.line)
 	assert.Equal(t, 4, s.col)
 }
+
+func TestScan_2(t *testing.T) {
+	s := source{
+		runes: []rune("Scarlet"),
+	}
+
+	assert.Panics(t, func() {
+		s.scan(0, token.UNDEFINED)
+	})
+}
+
+func TestScan_3(t *testing.T) {
+	s := source{
+		runes: []rune("Scarlet"),
+	}
+
+	assert.Panics(t, func() {
+		s.scan(99, token.UNDEFINED)
+	})
+}
