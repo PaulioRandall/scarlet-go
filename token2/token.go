8e7b1a7a826bb3a35fd12e4013b339dbdf40de27
PaulioRandall <29413800+PaulioRandall@users.noreply.github.com>
2019-12-21 00:17:04 +0000

    Reduce token struct & functionality but don't integrate

---
+++ token2/token.go
@@ -0,0 +1,47 @@
+package token2
+
+import (
+	"fmt"
+)
+
+// Kind represents a token type.
+type Kind string
+
+const (
+	UNDEFINED Kind = ``
+	// ------------------
+	COMMENT     Kind = `COMMENT`
+	WHITESPACE  Kind = `WHITESPACE`
+	NEWLINE     Kind = `NEWLINE`
+	FUNC        Kind = `FUNC`
+	DO          Kind = `DO`
+	END         Kind = `END`
+	ID          Kind = `ID`
+	ID_DELIM    Kind = `ID_DELIM`
+	ASSIGN      Kind = `ASSIGN`
+	OPEN_PAREN  Kind = `OPEN_PAREN`
+	CLOSE_PAREN Kind = `CLOSE_PAREN`
+	SPELL       Kind = `SPELL`
+	STR_LITERAL Kind = `STR_LITERAL`
+)
+
+// Token represents a grammer token within a source file.
+type Token struct {
+	Kind  Kind
+	Value string
+	Line  int
+}
+
+// NewToken creates a new token.
+func NewToken(k Kind, v string, l int) Token {
+	return Token{
+		Kind:  k,
+		Value: v,
+		Line:  l,
+	}
+}
+
+// String returns a string representation of the token.
+func (t Token) String() string {
+	return fmt.Sprintf(`%d: '%s' (%s)`, t.Line, t.Value, t.Kind)
+}
