46e83ba61af7c394ed65ab9fd401c15a2812c9d1
PaulioRandall <29413800+PaulioRandall@users.noreply.github.com>
2019-12-29 22:15:33 +0000

    Create simplified testing utilities in parser pkg
    I've decided it would be better to split matching and the creation
    of an evaluation or parse tree, which ever it ended up being.

---
+++ parser/testing_2.go
@@ -0,0 +1,36 @@
+package parser
+
+import (
+	"testing"
+
+	"github.com/PaulioRandall/scarlet-go/lexor"
+	"github.com/PaulioRandall/scarlet-go/token"
+
+	"github.com/stretchr/testify/require"
+)
+
+type matcher_2 func(tc *TokenCollector) int
+
+func dummyTC_2(stream []token.Token) *TokenCollector {
+	st := lexor.DummyScanToken(stream)
+	tr := NewTokenReader(st)
+	return NewTokenCollector(tr)
+}
+
+func testMatcher_2(
+	t *testing.T,
+	exp int,
+	err bool,
+	f matcher_2,
+	in ...token.Token,
+) {
+
+	tc := dummyTC(in)
+
+	if err {
+		require.Panics(t, func() { f(tc) }, "Expected a panic")
+		return
+	}
+
+	require.Equal(t, exp, f(tc), "Expected %d matched tokens", exp)
+}
