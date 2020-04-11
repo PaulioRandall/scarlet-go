1426fb96e963653ba824636a53406224fff8cf1e
PaulioRandall <29413800+PaulioRandall@users.noreply.github.com>
2020-04-11 09:52:00 +0100

    Save

--- streams/scanner/nonTerminals.go
+++ streams/scanner/nonTerminals.go
@@ -19,37 +19,6 @@ type nonTerminal struct {
 // beginning of the array to ensure the correct token is scanned.
 func nonTerminals() []nonTerminal {
 	return []nonTerminal{
-		// TODO: number literals
-		// TODO: string literals
-		// TODO: string templates
-		nonTerminal{lexeme.LEXEME_FLOAT, func(ss symbol.SymbolStream) int {
-
-			const (
-				DELIM     = "."
-				DELIM_LEN = len(DELIM)
-			)
-
-			n := integerMatcher(ss, 0)
-
-			if n == 0 || n == ss.Len() || !ss.IsMatch(n, DELIM) {
-				return 0
-			}
-
-			fractionalLen := integerMatcher(ss, n+DELIM_LEN)
-
-			if fractionalLen == 0 {
-				// One or many fractional digits must follow a delimiter. Zero following
-				// digits is invalid syntax, so we must panic.
-				panic(terror(ss, n+DELIM_LEN,
-					"Invalid syntax, expected digit after decimal point",
-				))
-			}
-
-			return n + DELIM_LEN + fractionalLen
-		}},
-		nonTerminal{lexeme.LEXEME_INT, func(ss symbol.SymbolStream) int {
-			return integerMatcher(ss, 0)
-		}},
 		nonTerminal{lexeme.LEXEME_NEWLINE, func(ss symbol.SymbolStream) int {
 			return ss.CountNewlineSymbols(0)
 		}},
@@ -170,6 +139,100 @@ func nonTerminals() []nonTerminal {
 		nonTerminal{lexeme.LEXEME_MT, func(ss symbol.SymbolStream) int {
 			return stringMatcher(ss, ">")
 		}},
+		// TODO: string templates
+		nonTerminal{lexeme.LEXEME_STRING, func(ss symbol.SymbolStream) int {
+
+			const (
+				PREFIX     = "`"
+				SUFFIX     = "`"
+				PREFIX_LEN = len(PREFIX)
+				SUFFIX_LEN = len(SUFFIX)
+			)
+
+			if !ss.IsMatch(0, PREFIX) {
+				return 0
+			}
+
+			n := ss.CountSymbolsWhile(0, func(i int, ru rune) bool {
+
+				if ss.IsMatch(i+PREFIX_LEN, SUFFIX) {
+					return false
+				}
+
+				checkForMissingTermination(ss, i)
+				return true
+			})
+
+			return PREFIX_LEN + n + SUFFIX_LEN
+		}},
+		nonTerminal{lexeme.LEXEME_TEMPLATE, func(ss symbol.SymbolStream) int {
+			// As the name suggests, templates can be populated with the value of
+			// identifiers, but the scanner is not concerned with parsing these. It does
+			// need to watch out for escaped terminals that also represent the string
+			// closer (suffix).
+
+			const (
+				PREFIX          = `"`
+				SUFFIX          = `"`
+				ESCAPE_TERMINAL = `/`
+				PREFIX_LEN      = len(PREFIX)
+				SUFFIX_LEN      = len(SUFFIX)
+			)
+
+			if !ss.IsMatch(0, PREFIX) {
+				return 0
+			}
+
+			var prevEscaped bool
+
+			n := ss.CountSymbolsWhile(0, func(i int, ru rune) bool {
+
+				escaped := prevEscaped
+				prevEscaped = false
+
+				if ss.IsMatch(i, ESCAPE_TERMINAL) {
+					prevEscaped = !escaped
+					return true
+				}
+
+				if !escaped && ss.IsMatch(i+PREFIX_LEN, SUFFIX) {
+					return false
+				}
+
+				checkForMissingTermination(ss, i)
+				return true
+			})
+
+			return PREFIX_LEN + n + SUFFIX_LEN
+		}},
+		nonTerminal{lexeme.LEXEME_FLOAT, func(ss symbol.SymbolStream) int {
+
+			const (
+				DELIM     = "."
+				DELIM_LEN = len(DELIM)
+			)
+
+			n := integerMatcher(ss, 0)
+
+			if n == 0 || n == ss.Len() || !ss.IsMatch(n, DELIM) {
+				return 0
+			}
+
+			fractionalLen := integerMatcher(ss, n+DELIM_LEN)
+
+			if fractionalLen == 0 {
+				// One or many fractional digits must follow a delimiter. Zero following
+				// digits is invalid syntax, so we must panic.
+				panic(terror(ss, n+DELIM_LEN,
+					"Invalid syntax, expected digit after decimal point",
+				))
+			}
+
+			return n + DELIM_LEN + fractionalLen
+		}},
+		nonTerminal{lexeme.LEXEME_INT, func(ss symbol.SymbolStream) int {
+			return integerMatcher(ss, 0)
+		}},
 	}
 }
 
@@ -199,4 +262,18 @@ func integerMatcher(ss symbol.SymbolStream, start int) int {
 	return ss.CountSymbolsWhile(start, func(_ int, ru rune) bool {
 		return unicode.IsDigit(ru)
 	})
+}
+
+func checkForMissingTermination(ss symbol.SymbolStream, i int) {
+	if ss.IsNewline(i) {
+		panic(terror(ss, 0,
+			"Newline encountered before a string or template was terminated",
+		))
+	}
+
+	if i+1 == ss.Len() {
+		panic(terror(ss, 0,
+			"EOF encountered before a string or template was terminated",
+		))
+	}
 }
--- streams/scanner/scanner.go
+++ streams/scanner/scanner.go
@@ -6,18 +6,12 @@
 // a focus on readability was more important. Also, each script is only scanned
 // once per execution so optimisation will probably not have any meaningful
 // effect.
-// 2. The terminal symbols used to represent various tokens have been separated
-// into the lexeme package, even though the ordering of scanning functions
-// depend on lexeme representations. This was conscious. It may not be possible
-// to effectivly separate them.
 //
 // This package is responsible for scanning scripts only, evaluation is
 // performed by the streams/evaluator package.
 //
 // TODO: Error handling needs to be simplified and the logger renamed to
 // 			 something more meaningful.
-// TODO: Some of these functions could probably do with being rewritten for
-//       greater clarity.
 package scanner
 
 import (
@@ -48,12 +42,6 @@ func New(s string) token.TokenStream {
 	}
 }
 
-// scanFunc is the common signiture used by every scanning function used by
-// the Read function. If a concrete scanning function finds a match it must
-// return a non-zero token and true as the match else it must return a zero
-// token along with false.
-type scanFunc func() (lexeme.Token, bool)
-
 // Read satisfies the TokenStream interface.
 func (sc *Scanner) Read() lexeme.Token {
 
@@ -66,140 +54,34 @@ func (sc *Scanner) Read() lexeme.Token {
 		}
 	}
 
-	// For proper parsing, the correct ordering of scanning functions may be
-	// important depending on the lexeme representations. These can be found in
-	// the lexeme package. Currently the followingering is required:
-	// 1. scanComment before scanSymbol
-	// 2. scanSymbol before scanWord
-	fs := []scanFunc{
-		sc.scanSymbol,         // DO, END, :=, +, <, etc
-		sc.scanStringLiteral,  // `literal`
-		sc.scanStringTemplate, // "Template: {identifier}"
+	tk := sc.parseNextToken()
+
+	if tk == (lexeme.Token{}) {
+		panic(terror(sc, 0, "Could not identify next token"))
 	}
 
-	// Iterate each function list calling one at a time until one of them
-	// identifies and parses the token.
-	for _, f := range fs {
-		if tk, match := f(); match {
-			return tk
-		}
-	}
-
-	panic(terror(sc, 0, "Could not identify next token"))
+	return tk
 }
 
-// scanSymbol implements the scanFunc signiture to return a non-zero token if
-// a simple math, logic, or control symbol (lone symbol, non-terminal) appears
-// next within the script, i.e. :=, +, <, etc.
-func (sc *Scanner) scanSymbol() (_ lexeme.Token, _ bool) {
+// parseNextToken attempts to match one of the non-terminal patterns to the next
+// set of terminals in the script. If found, the terminals are removed and used
+// to create a token.
+func (sc *Scanner) parseNextToken() (_ lexeme.Token) {
 
-	for _, nt := range nonTerminals() {
+	matchers := nonTerminals()
+	size := len(matchers)
 
-		matchLen := nt.Matcher(symbol.SymbolStream(sc))
+	for i := 0; i < size; i++ {
 
-		if matchLen > 0 {
-			tk := sc.tokenize(matchLen, nt.Lexeme)
-			return tk, true
+		nonTerminal := matchers[i]
+		n := nonTerminal.Matcher(symbol.SymbolStream(sc))
+
+		if n > 0 {
+			return sc.tokenize(n, nonTerminal.Lexeme)
 		}
 	}
 
 	return
-}
-
-// scanStringLiteral implements the scanFunc signiture to return a non-zero
-// token if a literal string appears next within the script.
-func (sc *Scanner) scanStringLiteral() (_ lexeme.Token, _ bool) {
-
-	const (
-		PREFIX = lexeme.STRING_SYMBOL_START
-		SUFFIX = lexeme.STRING_SYMBOL_END
-	)
-
-	n := sc.CountSymbolsWhile(0, func(i int, _ rune) bool {
-
-		if i == 0 {
-			// If the initial terminal does not signify a string literal then exit
-			// straight away, n will be 0.
-			return sc.IsMatch(i, PREFIX)
-		}
-
-		switch { // We must panic if we can't find the end of the string.
-		case sc.IsMatch(i, SUFFIX):
-			return false
-		case sc.IsNewline(i):
-			panic(terror(sc, 0,
-				"Newline encountered before a string literal was terminated",
-			))
-		case i+1 == sc.Len():
-			panic(terror(sc, 0,
-				"EOF encountered before a string literal was terminated",
-			))
-		}
-
-		return true
-	})
-
-	if n == 0 {
-		return
-	}
-
-	tk := sc.tokenize(n+1, lexeme.LEXEME_STRING)
-	return tk, true
-}
-
-// scanStringTemplate implements the scanFunc signiture to return a non-zero
-// token if a template string appears next within the script. As the name
-// suggests, templates can be populated with the value of identifiers, but the
-// scanner is not concerned with parsing these. It does need to watch out for
-// escaped terminals that also represent the string closer (suffix).
-func (sc *Scanner) scanStringTemplate() (_ lexeme.Token, _ bool) {
-
-	const (
-		PREFIX        = lexeme.TEMPLATE_SYMBOL_START
-		SUFFIX        = lexeme.TEMPLATE_SYMBOL_END
-		SUFFIX_LEN    = len(SUFFIX)
-		ESCAPE_SYMBOL = lexeme.TEMPLATE_SYMBOL_ESCAPE
-	)
-
-	var prevEscaped bool
-
-	n := sc.CountSymbolsWhile(0, func(i int, _ rune) bool {
-
-		escaped := prevEscaped
-		prevEscaped = false
-
-		if i == 0 {
-			// If the initial terminal does not signify a string template then exit
-			// straight away, n will be 0.
-			return sc.IsMatch(i, PREFIX)
-		}
-
-		switch { // We must panic if we can't find the end of the template.
-		case sc.IsMatch(i, ESCAPE_SYMBOL):
-			prevEscaped = true
-			return true
-		case !escaped && sc.IsMatch(i, SUFFIX): // Ensure the suffix is not escaped
-			return false
-		case sc.IsNewline(i):
-			panic(terror(sc, 0,
-				"Newline encountered before a string template was terminated",
-			))
-		case i+1 == sc.Len():
-			panic(terror(sc, 0,
-				"EOF encountered before a string template was terminated",
-			))
-		}
-
-		return true
-	})
-
-	if n == 0 {
-		return
-	}
-
-	n += SUFFIX_LEN
-	tk := sc.tokenize(n, lexeme.LEXEME_TEMPLATE)
-	return tk, true
 }
 
 // tokenize creates a new token from the next non-terminal. It reads off n
@@ -213,7 +95,6 @@ func (sc *Scanner) tokenize(n int, lex lexeme.Lexeme) lexeme.Token {
 	}
 
 	tk.Value = sc.SymbolStream.Slice(n, lex == lexeme.LEXEME_NEWLINE)
-
 	return tk
 }
 
