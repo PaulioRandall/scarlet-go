f6acc93942098c33a1c9c6e372c0780f17c2dae1
PaulioRandall <29413800+PaulioRandall@users.noreply.github.com>
2020-04-14 19:14:36 +0100

    Save

--- streams/runtime/value.go
+++ streams/runtime/value.go
@@ -1,10 +1,9 @@
 package runtime
 
 import (
-//	"fmt"
-//"strconv"
-
-//"github.com/PaulioRandall/scarlet-go/lexeme"
+	//	"fmt"
+	"strconv"
+	//"github.com/PaulioRandall/scarlet-go/lexeme"
 )
 
 // Value represents a value within the executing program, either the value of
@@ -23,6 +22,56 @@ func (_ Void) Get() interface{} {
 
 func (_ Void) String() string {
 	return "(VOID) _"
+}
+
+type Bool bool
+
+func (b Bool) Get() interface{} {
+	return bool(b)
+}
+
+func (b Bool) String() string {
+	return "(BOOL) " + strconv.FormatBool(bool(b))
+}
+
+type Int int64
+
+func (i Int) Get() interface{} {
+	return int64(i)
+}
+
+func (i Int) String() string {
+	return "(INT) " + strconv.FormatInt(int64(i), 10)
+}
+
+type Float float64
+
+func (f Float) Get() interface{} {
+	return float64(f)
+}
+
+func (f Float) String() string {
+	return "(FLOAT) " + strconv.FormatFloat(float64(f), 'f', -1, 64)
+}
+
+type String string
+
+func (s String) Get() interface{} {
+	return string(s)
+}
+
+func (s String) String() string {
+	return "(STRING) " + string(s)
+}
+
+type Template string
+
+func (t Template) Get() interface{} {
+	return string(t)
+}
+
+func (t Template) String() string {
+	return "(TEMPLATE) " + string(t)
 }
 
 /*
