782467e8cd28d17c7f3ec06648c1345a9d695cbd
PaulioRandall <29413800+PaulioRandall@users.noreply.github.com>
2020-04-24 20:42:15 +0100

    Save

---
+++ pkg/statement/list.go
@@ -0,0 +1,71 @@
+package statement
+
+import (
+	"github.com/PaulioRandall/scarlet-go/pkg/token"
+)
+
+type List struct {
+	Key   token.Token
+	Open  token.Token
+	Exprs []Expression
+	Close token.Token
+}
+
+func (l List) Token() token.Token {
+	return l.Key
+}
+
+func (l List) String(i int) string {
+
+	var s str
+
+	s.indent(i).
+		append("[List] ").
+		append(l.Key.String())
+
+	s.newline().
+		indent(i + 1).
+		append("Open: ").
+		append(l.Open.String())
+
+	s.newline().
+		indent(i + 1).
+		append("Exprs:")
+
+	s.newline().
+		appendExps(i+2, l.Exprs)
+
+	s.newline().
+		indent(i + 1).
+		append("Close: ").
+		append(l.Close.String())
+
+	return s.String()
+}
+
+type ListAccess struct {
+	ID    Identifier
+	Index Expression
+}
+
+func (la ListAccess) Token() token.Token {
+	return la.ID.Source
+}
+
+func (la ListAccess) String(i int) string {
+
+	var s str
+
+	s.indent(i).
+		append("[ListAccess] ").
+		append(la.ID.Source.String())
+
+	s.newline().
+		indent(i + 1).
+		append("Index:")
+
+	s.newline().
+		append(la.Index.String(i + 2))
+
+	return s.String()
+}
