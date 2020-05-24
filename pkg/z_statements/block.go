5fea235b770e315ee9b6a7e0602711c3e8b037f7
PaulioRandall <29413800+PaulioRandall@users.noreply.github.com>
2020-05-24 08:59:00 +0100

    Save

---
+++ pkg/z_statements/block.go
@@ -0,0 +1,42 @@
+package z_statement
+
+import (
+	. "github.com/PaulioRandall/scarlet-go/pkg/z_token"
+)
+
+type Block struct {
+	Open  Token
+	Stats []Statement
+	Close Token
+}
+
+func (b Block) Token() Token {
+	return b.Open
+}
+
+func (b Block) String(i int) string {
+
+	var s str
+
+	s.indent(i).
+		append("[Block]")
+
+	s.newline().
+		indent(i + 1).
+		append("Open: ").
+		appendTk(b.Open)
+
+	s.newline().
+		indent(i + 1).
+		append("Statements:")
+
+	s.newline().
+		appendStats(i+2, b.Stats)
+
+	s.newline().
+		indent(i + 1).
+		append("Close: ").
+		appendTk(b.Close)
+
+	return s.String()
+}
