1eab42bf4cc57e95076e571605bdb7fa6c6edca3
PaulioRandall <29413800+PaulioRandall@users.noreply.github.com>
2020-05-29 23:25:22 +0100

    Save

---
+++ pkg/parsers/new_recursive/recursive.go
@@ -0,0 +1,13 @@
+package recursive
+
+/*
+import (
+	. "github.com/PaulioRandall/scarlet-go/pkg/statement"
+	. "github.com/PaulioRandall/scarlet-go/pkg/token"
+)
+
+func ParseAll(tks []Token) []Statement {
+	p := &pipe{NewIterator(tks)}
+	return parseStatements(p)
+}
+*/
---
+++ pkg/parsers/new_tests/tests.go
@@ -0,0 +1,59 @@
+package tests
+
+import (
+	"strconv"
+	"testing"
+
+	. "github.com/PaulioRandall/scarlet-go/pkg/statement"
+	. "github.com/PaulioRandall/scarlet-go/pkg/token"
+
+	"github.com/stretchr/testify/require"
+)
+
+type ParseFunc func(in []Token) []Statement
+type TestFunc func(t *testing.T, pf ParseFunc)
+
+func tok(m Morpheme, v string) Token {
+	return NewToken(m, v, 0, 0)
+}
+
+func Run(t *testing.T, pf ParseFunc, tf TestFunc) {
+
+	defer func() {
+		if e := recover(); e != nil {
+			v, _ := e.(error)
+			require.Nil(t, e, v.Error())
+		}
+	}()
+
+	tf(t, pf)
+}
+
+func expectOneStat(t *testing.T, exp Statement, acts []Statement) {
+	expectSize(t, 1, acts)
+	expectStat(t, exp, acts[0])
+}
+
+func expectStat(t *testing.T, exp, act Statement) {
+	require.Equal(t, exp, act,
+		"Expect: "+exp.String(0)+"\n"+
+			"Actual: "+act.String(0),
+	)
+}
+
+func expectSize(t *testing.T, exp int, acts []Statement) {
+	require.Equal(t, exp, len(acts),
+		"Expected "+strconv.Itoa(exp)+
+			" statement but got "+strconv.Itoa(len(acts)))
+}
+
+func expectPanic(t *testing.T, f func()) {
+	require.Panics(t, f, "Expected a panic")
+}
+
+func stStr(sts []Statement, i int) string {
+	if i < len(sts) {
+		return sts[i].String(1)
+	}
+	return ""
+}
