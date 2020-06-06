89b9143d1b5a2e5d1fa652788ea1219ab0705d45
PaulioRandall <29413800+PaulioRandall@users.noreply.github.com>
2020-06-06 17:00:18 +0100

    Save

--- pkg/scanners/new_tests/tests.go
+++ pkg/scanners/new_tests/tests.go
@@ -1,7 +1,6 @@
 package new_tests
 
 import (
-	"strconv"
 	"testing"
 
 	. "github.com/PaulioRandall/scarlet-go/pkg/token"
@@ -9,19 +8,15 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-type ScanFunc func(in string) []Token
-type TestFunc func(t *testing.T, sf ScanFunc)
+func assertError(t *testing.T, toks []Token, e error) {
+	require.NotNil(t, e, "Expected an error for this test")
+	require.Nil(t, toks, "Upon error, resultant token slice should be nil")
+}
 
-func Run(t *testing.T, sf ScanFunc, tf TestFunc) {
-
-	defer func() {
-		if e := recover(); e != nil {
-			v, _ := e.(error)
-			require.Nil(t, e, v.Error())
-		}
-	}()
-
-	tf(t, sf)
+func assertResults(t *testing.T, exp, toks []Token, e error) {
+	require.Nil(t, e, "Did not expect an error for this test")
+	require.NotNil(t, toks, "SANITY CHECK! What tokens were expected?")
+	require.NotNil(t, toks, "Expected a non-nil token slice")
 }
 
 func checkMany(t *testing.T, exps, acts []Token) {
@@ -32,46 +27,19 @@ func checkMany(t *testing.T, exps, acts []Token) {
 	for i := 0; i < expSize || i < actSize; i++ {
 
 		require.True(t, i < actSize,
-			"Expected ("+tkStr(exps, i)+") but no actual tokens remain")
+			"Expected ("+tkStr(exps, i)+"), but no actual tokens remain")
 
 		require.True(t, i < expSize,
-			"Didn't expect any more tokens but got ("+tkStr(acts, i)+")")
+			"Did not expect any more tokens, but got ("+tkStr(acts, i)+")")
 
 		checkToken(t, exps[i], acts[i])
 	}
 }
 
-func checkOne(t *testing.T, exp Token, acts []Token) {
-	checkSize(t, 1, acts)
-	checkToken(t, exp, acts[0])
-}
-
-func checkFirstNot(t *testing.T, notExp Token, acts []Token) {
-	checkMinSize(t, 1, acts)
-	require.NotEqual(t, notExp, acts[0],
-		"Expected any token except ("+ToString(notExp)+") but got it")
-}
-
-func checkSize(t *testing.T, exp int, acts []Token) {
-	require.Equal(t, exp, len(acts),
-		"Expected "+strconv.Itoa(exp)+
-			" tokens (inc EOF) but got "+strconv.Itoa(len(acts)))
-}
-
-func checkMinSize(t *testing.T, min int, acts []Token) {
-	require.True(t, min <= len(acts),
-		"Expected minimum "+strconv.Itoa(min)+
-			" tokens (inc EOF) but got "+strconv.Itoa(len(acts)))
-}
-
-func checkPanic(t *testing.T, f func()) {
-	require.Panics(t, f, "Expected a panic")
-}
-
 func checkToken(t *testing.T, exp, act Token) {
-	require.NotNil(t, act, "Expected token ("+ToString(exp)+") got nil")
+	require.NotNil(t, act, "Expected token ("+ToString(exp)+"), but got nil")
 
-	m := "Expected (" + ToString(exp) + ") but got (" + ToString(act) + ")"
+	m := "Expected (" + ToString(exp) + "), but got (" + ToString(act) + ")"
 
 	require.Equal(t, exp.Morpheme(), act.Morpheme(), m)
 	require.Equal(t, exp.Value(), act.Value(), m)
