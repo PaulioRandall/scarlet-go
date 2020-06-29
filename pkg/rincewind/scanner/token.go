b1e4682766085b9481cacebdd0ae543cf0039f47
PaulioRandall <29413800+PaulioRandall@users.noreply.github.com>
2020-06-30 00:00:13 +0100

    Save

---
+++ pkg/rincewind/scanner/token.go
@@ -0,0 +1,74 @@
+package token
+
+import (
+	"fmt"
+
+	. "github.com/PaulioRandall/scarlet-go/pkg/rincewind/token"
+)
+
+func newToken(
+	gt GenType,
+	st SubType,
+	raw string,
+	val interface{},
+	line, col int,
+) Token {
+
+	return tok{
+		gt:       gt,
+		st:       st,
+		raw:      raw,
+		val:      val,
+		line:     line,
+		colBegin: col,
+		colEnd:   col + len(raw),
+	}
+}
+
+type tok struct {
+	gt       GenType
+	st       SubType
+	raw      string
+	val      interface{}
+	line     int
+	colBegin int
+	colEnd   int
+}
+
+func (tk tok) GenType() GenType {
+	return tk.gt
+}
+
+func (tk tok) SubType() SubType {
+	return tk.st
+}
+
+func (tk tok) Raw() string {
+	return tk.raw
+}
+
+func (tk tok) Val() interface{} {
+	return tk.val
+}
+
+func (tk tok) Begin() (int, int) {
+	return tk.line, tk.colBegin
+}
+
+func (tk tok) End() (int, int) {
+	return tk.line, tk.colEnd
+}
+
+func (tk tok) String() string {
+
+	// +1 converts from line index to number
+	return fmt.Sprintf(`%d:%d %d:%d %s:%s %q`,
+		tk.line+1,
+		tk.colBegin,
+		tk.line+1,
+		tk.colEnd,
+		tk.gt.String(),
+		tk.st.String(),
+		tk.val,
+	)
+}
--- pkg/rincewind/token/token.go
+++ pkg/rincewind/token/token.go
@@ -5,78 +5,99 @@ import (
 )
 
 type Token interface {
-	fmt.Stringer
 	GenType() GenType
 	SubType() SubType
 	Raw() string
 	Val() interface{}
+	Snippet
+	fmt.Stringer
+}
+
+type Snippet interface {
 	Begin() (int, int)
 	End() (int, int)
 }
 
-func NewToken(
-	gt GenType,
-	st SubType,
-	raw string,
-	val interface{},
-	line, col int,
-) Token {
+type GenType int // General type
+type SubType int
 
-	return tok{
-		gt:       gt,
-		st:       st,
-		raw:      raw,
-		val:      val,
-		line:     line,
-		colBegin: col,
-		colEnd:   col + len(raw),
+func (gt GenType) String() string {
+	return genTypes[gt]
+}
+
+func (st SubType) String() string {
+	return subTypes[st]
+}
+
+const (
+	GT_UNDEFINED GenType = 0
+	ST_UNDEFINED SubType = 0
+	// ------------------
+	GT_WHITESPACE GenType = iota
+	// ------------------
+	GT_TERMINATOR GenType = iota
+	ST_TERMINATOR SubType = iota
+	ST_NEWLINE
+	// ------------------
+	GT_IDENTIFIER GenType = iota
+	ST_IDENTIFIER SubType = iota
+	ST_VOID
+	// ------------------
+	GT_LITERAL GenType = iota
+	ST_BOOL    SubType = iota
+	ST_NUMBER
+	ST_STRING
+	// ------------------
+	GT_SPELL GenType = iota
+	// ------------------
+	GT_BRACKET    GenType = iota
+	ST_PAREN_OPEN SubType = iota
+	ST_PAREN_CLOSE
+)
+
+var genTypes = map[GenType]string{
+	GT_WHITESPACE: `whitespace`,
+	GT_TERMINATOR: `terminator`,
+	GT_IDENTIFIER: `identifier`,
+	GT_LITERAL:    `literal`,
+	GT_SPELL:      `spell`,
+	GT_BRACKET:    `bracket`,
+}
+
+var subTypes = map[SubType]string{
+	ST_IDENTIFIER:  `identifier`,
+	ST_VOID:        `void`,
+	ST_TERMINATOR:  `terminator`,
+	ST_NEWLINE:     `newline`,
+	ST_BOOL:        `bool`,
+	ST_NUMBER:      `number`,
+	ST_STRING:      `string`,
+	ST_PAREN_OPEN:  `paren_open`,
+	ST_PAREN_CLOSE: `paren_close`,
+}
+
+/*
+func (ty TokenType) Precedence() int {
+	switch ty {
+	case TK_MULTIPLY, TK_DIVIDE, TK_REMAINDER:
+		return 6 // Multiplicative
+
+	case TK_PLUS, TK_MINUS:
+		return 5 // Additive
+
+	case TK_LESS_THAN, TK_LESS_THAN_OR_EQUAL, TK_MORE_THAN, TK_MORE_THAN_OR_EQUAL:
+		return 4 // Relational
+
+	case TK_EQUAL, TK_NOT_EQUAL:
+		return 3 // Equalitive
+
+	case TK_AND:
+		return 2
+
+	case TK_OR:
+		return 1
 	}
-}
 
-type tok struct {
-	gt       GenType
-	st       SubType
-	raw      string
-	val      interface{}
-	line     int
-	colBegin int
-	colEnd   int
+	return 0
 }
-
-func (tk tok) GenType() GenType {
-	return tk.gt
-}
-
-func (tk tok) SubType() SubType {
-	return tk.st
-}
-
-func (tk tok) Raw() string {
-	return tk.raw
-}
-
-func (tk tok) Val() interface{} {
-	return tk.val
-}
-
-func (tk tok) Begin() (int, int) {
-	return tk.line, tk.colBegin
-}
-
-func (tk tok) End() (int, int) {
-	return tk.line, tk.colEnd
-}
-
-func (tk tok) String() string {
-
-	// +1 converts from line index to number
-	return fmt.Sprintf(`%d:%d %d:%d %s:%s %q`,
-		tk.line+1,
-		tk.colBegin,
-		tk.line+1,
-		tk.colEnd,
-		tk.gt.String(),
-		tk.st.String(),
-		tk.val,
-	)
-}
+*/
