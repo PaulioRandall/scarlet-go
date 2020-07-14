e20bf5021f57e3dd9fef08342763e8f970a3649e
PaulioRandall <29413800+PaulioRandall@users.noreply.github.com>
2020-07-14 19:19:51 +0100

    Save

--- pkg/program/run.go
+++ pkg/program/run.go
@@ -2,71 +2,38 @@ package program
 
 import (
 	"fmt"
-	"io/ioutil"
 	"os"
 
-	"github.com/PaulioRandall/scarlet-go/pkg/esmerelda/a_scan"
-	"github.com/PaulioRandall/scarlet-go/pkg/esmerelda/b_sanitise"
-	"github.com/PaulioRandall/scarlet-go/pkg/esmerelda/c_check"
-	"github.com/PaulioRandall/scarlet-go/pkg/esmerelda/d_shunt"
-	"github.com/PaulioRandall/scarlet-go/pkg/esmerelda/e_compile"
 	"github.com/PaulioRandall/scarlet-go/pkg/esmerelda/f_runtime"
+	"github.com/PaulioRandall/scarlet-go/pkg/esmerelda/shared/inst"
 )
 
 func run(args []string) error {
-	return runEsme()
-}
 
-func runEsme() error { // Run it with `./godo run`
-
-	exitCode, e := esme("test.scarlet")
-	fmt.Println()
-
+	ins, e := build(args)
 	if e != nil {
-		fmt.Printf("[ERROR] %+v\n", e)
+		const BUILD_ERROR_CODE = 1
+		err(BUILD_ERROR_CODE, e)
 	}
 
-	fmt.Printf("Exit code: %d\n", exitCode)
+	exitCode, e := exeInstructions(ins)
+	if e != nil {
+		err(exitCode, e)
+	}
 
 	os.Exit(exitCode)
 	return nil
 }
 
-func esme(file string) (int, error) {
-
-	const ERROR_CODE = 1
-
-	s, e := ioutil.ReadFile(file)
-	if e != nil {
-		return ERROR_CODE, e
-	}
-
-	tks, e := scan.ScanAll(string(s))
-	if e != nil {
-		return ERROR_CODE, e
-	}
-
-	tks, e = sanitise.SanitiseAll(tks)
-	if e != nil {
-		return ERROR_CODE, e
-	}
-
-	tks, e = check.CheckAll(tks)
-	if e != nil {
-		return ERROR_CODE, e
-	}
-
-	tks, e = shunt.ShuntAll(tks)
-	if e != nil {
-		return ERROR_CODE, e
-	}
-
-	ins, e := compile.CompileAll(tks)
-	if e != nil {
-		return ERROR_CODE, e
-	}
-
+func exeInstructions(ins []inst.Instruction) (int, error) {
 	rt := runtime.New(ins)
 	rt.Start()
 	return rt.Env().ExitCode, rt.Env().Err
+}
+
+func err(exitCode int, e error) {
+	fmt.Println()
+	fmt.Printf("[ERROR] %d\n", exitCode)
+	fmt.Printf("%+v\n", e)
+	os.Exit(exitCode)
 }
