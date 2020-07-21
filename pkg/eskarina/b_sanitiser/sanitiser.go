a04e1e6e4f544ca50ee67dee45e6ec54de91af5b
PaulioRandall <29413800+PaulioRandall@users.noreply.github.com>
2020-07-21 18:25:32 +0100

    Save

--- pkg/eskarina/b_sanitiser/sanitiser.go
+++ pkg/eskarina/b_sanitiser/sanitiser.go
@@ -2,27 +2,52 @@ package sanitiser
 
 import (
 	"github.com/PaulioRandall/scarlet-go/pkg/eskarina/lexeme"
-	//"github.com/PaulioRandall/scarlet-go/pkg/eskarina/perror"
-	//"github.com/PaulioRandall/scarlet-go/pkg/eskarina/prop"
+	"github.com/PaulioRandall/scarlet-go/pkg/eskarina/prop"
 )
 
-func SanitiseAll(first *lexeme.Lexeme) (*lexeme.Lexeme, error) {
+func SanitiseAll(first *lexeme.Lexeme) *lexeme.Lexeme {
 
 	for lex := first; lex != nil; lex = lex.Next {
-
-		keep, e := sanitise(lex)
-		if e != nil {
-			return nil, e
-		}
-
-		if !keep {
-
+		if !sanitise(lex) {
+			first = lex
+			break
 		}
 	}
 
-	return nil, nil
+	for lex := first; lex != nil; lex = lex.Next {
+		sanitise(lex)
+	}
+
+	return first
 }
 
-func sanitise(lex *lexeme.Lexeme) (bool, error) {
-	return true, nil
+func sanitise(lex *lexeme.Lexeme) bool {
+
+	switch {
+	case lex.Has(prop.PR_REDUNDANT):
+		lex.Remove()
+
+	case lex.Prev == nil && lex.Has(prop.PR_TERMINATOR):
+		lex.Remove()
+
+	case lex.Prev == nil:
+		return false
+
+	case lex.Prev.Has(prop.PR_TERMINATOR) && lex.Has(prop.PR_TERMINATOR):
+		lex.Remove()
+
+	case lex.Prev.Is(prop.PR_PARENTHESIS, prop.PR_OPENER) && lex.Has(prop.PR_NEWLINE):
+		lex.Remove()
+
+	case lex.Prev.Has(prop.PR_SEPARATOR) && lex.Has(prop.PR_NEWLINE):
+		lex.Remove()
+
+	case lex.Prev.Has(prop.PR_SEPARATOR) && lex.Is(prop.PR_PARENTHESIS, prop.PR_CLOSER):
+		lex.Prev.Remove()
+
+	default:
+		return false
+	}
+
+	return true
 }
