8b5e351f3d6a5bf2708ef319c6812645988f20d2
PaulioRandall <29413800+PaulioRandall@users.noreply.github.com>
2020-07-23 23:30:14 +0100

    Save

---
+++ pkg/eskarina/ae_compiler/compile.go
@@ -0,0 +1,111 @@
+package compiler
+
+import (
+	"github.com/PaulioRandall/scarlet-go/pkg/eskarina/code"
+	"github.com/PaulioRandall/scarlet-go/pkg/eskarina/inst"
+	"github.com/PaulioRandall/scarlet-go/pkg/eskarina/lexeme"
+	"github.com/PaulioRandall/scarlet-go/pkg/eskarina/prop"
+)
+
+func CompileAll(head *lexeme.Lexeme) *inst.Instruction {
+
+	com := &compiler{
+		input: lexeme.NewContainer(head),
+		out:   &inst.Container{},
+	}
+
+	compile(com)
+	return com.out.Head()
+}
+
+func compile(com *compiler) {
+
+	defer com.reject() // GEN_TERMINATOR, now redundant
+
+	switch {
+	case com.empty():
+		com.unexpected()
+
+	case com.has(prop.PR_CALLABLE):
+		call(com)
+
+	default:
+		com.unexpected()
+	}
+}
+
+func call(com *compiler) {
+
+	com.take() // PR_PARAMETERS redundant
+	argCount := 0
+
+	for !com.has(prop.PR_SPELL) {
+		argCount++
+		expression(com)
+	}
+
+	lex := com.take()
+
+	com.output(&inst.Instruction{
+		Code:    code.CO_VAL_PUSH,
+		Data:    argCount,
+		Snippet: lex,
+	})
+
+	com.output(&inst.Instruction{
+		Code:    code.CO_SPELL,
+		Data:    lex.Raw[1:],
+		Snippet: lex,
+	})
+}
+
+func expression(com *compiler) {
+
+	switch {
+	case com.has(prop.PR_IDENTIFIER):
+		identifier(com)
+
+	case com.has(prop.PR_LITERAL):
+		literal(com)
+
+	default:
+		com.unexpected()
+	}
+}
+
+func identifier(com *compiler) {
+
+	lex := com.take()
+
+	com.output(&inst.Instruction{
+		Code:    code.CO_CTX_GET,
+		Data:    lex.Raw,
+		Snippet: lex,
+	})
+}
+
+func literal(com *compiler) {
+
+	lex := com.take()
+
+	in := &inst.Instruction{
+		Code:    code.CO_VAL_PUSH,
+		Snippet: lex,
+	}
+
+	switch {
+	case lex.Is(prop.PR_BOOL):
+		in.Data = lex.Raw == "true"
+
+	case lex.Is(prop.PR_NUMBER):
+		//in.Data = number.New(lex.Raw())
+
+	case lex.Is(prop.PR_STRING):
+		in.Data = lex.Raw
+
+	default:
+		com.unexpected()
+	}
+
+	com.output(in)
+}
--- pkg/eskarina/ae_compiler/compiler.go
+++ pkg/eskarina/ae_compiler/compiler.go
@@ -3,9 +3,41 @@ package compiler
 import (
 	"github.com/PaulioRandall/scarlet-go/pkg/eskarina/inst"
 	"github.com/PaulioRandall/scarlet-go/pkg/eskarina/lexeme"
+	"github.com/PaulioRandall/scarlet-go/pkg/eskarina/prop"
 )
 
-func CompileAll(head *lexeme.Lexeme) (*inst.Instruction, error) {
+type compiler struct {
+	input lexeme.Queue
+	out   inst.Queue
+}
 
-	return nil, nil
+func (com *compiler) empty() bool {
+	return com.input.Empty()
+}
+
+func (com *compiler) has(props ...prop.Prop) bool {
+	return com.input.More() && com.input.Head().Has(props...)
+}
+
+func (com *compiler) take() *lexeme.Lexeme {
+	return com.input.Take()
+}
+
+func (com *compiler) reject(props ...prop.Prop) {
+	if com.input.More() {
+		com.input.Take()
+	}
+}
+
+func (com *compiler) output(in *inst.Instruction) {
+	com.out.Put(in)
+}
+
+func (com *compiler) unexpected() {
+
+	if com.input.Empty() {
+		panic("Unexpected EOF")
+	}
+
+	panic("Unexpected token: " + com.input.Head().String())
 }
