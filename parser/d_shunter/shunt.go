3d13f0d95a220180e990ae4a2003c3024f6f361d
PaulioRandall <29413800+PaulioRandall@users.noreply.github.com>
2020-08-12 18:22:00 +0100

    Save

--- parser/d_shunter/shunt.go
+++ parser/d_shunter/shunt.go
@@ -21,37 +21,57 @@ func shunt(shy *shuntingYard) {
 	for shy.more() {
 
 		switch {
+		case shy.inQueue(lexeme.SPELL):
+			call(shy)
+
+		default:
+			panic("Unexpected token: " + shy.queue.Head().String())
+		}
+
+		if shy.queueTok().IsTerminator() {
+			shy.output()
+			continue
+		}
+
+		panic("Unexpected token: " + shy.queue.Head().String())
+	}
+}
+
+func call(shy *shuntingYard) {
+
+	shy.push() // @Spell
+	shy.push() // "("
+	shy.emit(*shy.stack.Top(), lexeme.CALLABLE)
+
+	if !shy.inQueue(lexeme.RIGHT_PAREN) {
+		arguments(shy)
+	}
+
+	shy.discard() // ")"
+	shy.eject()   // "("
+	shy.pop()     // @Spell
+}
+
+func arguments(shy *shuntingYard) {
+
+	for !shy.inQueue(lexeme.RIGHT_PAREN) {
+
+		if shy.inQueue(lexeme.SEPARATOR) {
+			//shy.output()
+			shy.discard()
+		}
+
+		argument(shy)
+	}
+}
+
+func argument(shy *shuntingYard) {
+
+	for !shy.inQueue(lexeme.SEPARATOR) && !shy.inQueue(lexeme.RIGHT_PAREN) {
+
+		switch {
 		case shy.queueTok().IsTerm():
 			shy.output()
-
-		case shy.inQueue(lexeme.SEPARATOR):
-			if shy.inStack(lexeme.LEFT_PAREN) {
-				shy.discard() // ","
-				break
-			}
-
-			shy.pop()
-
-		case shy.inQueue(lexeme.SPELL):
-			shy.push() // @Spell
-			shy.push() // "("
-			shy.emit(*shy.stack.Top(), lexeme.CALLABLE)
-
-		case shy.inQueue(lexeme.RIGHT_PAREN):
-			if shy.inStack(lexeme.LEFT_PAREN) {
-				shy.discard() // ")"
-				shy.eject()   // "("
-			}
-
-			shy.pop() // @Spell
-
-		case shy.queueTok().IsTerminator():
-			if shy.stack.Empty() {
-				shy.output()
-				break
-			}
-
-			fallthrough
 
 		default:
 			panic("Unexpected token: " + shy.queue.Head().String())
